95ee2b027f2f7109c383eccc415e9585c3159b19
kamiya555 <kamiya.aka@gmail.com>
2016-11-26 00:16:40 +0900

    Create charts by dividing logic.

---
+++ app/scripts/LanguageChart.js
@@ -0,0 +1,86 @@
+"use strict";
+
+var util = require('./util');
+var LanguageModel = require('./LanguageModel');
+
+var LanguageChart = function() {
+
+  function _createCanvasWithRelationalElements() {
+    var divWrapper = document.createElement('div');
+
+    // show chartblock title
+    var chartBlockTitle = document.createElement('h2');
+    chartBlockTitle.className = 'f4 mb-2 text-normal';
+    chartBlockTitle.innerHTML = 'Language chart';
+    divWrapper.appendChild(chartBlockTitle);
+
+    // create chart canvas
+    var chartOuterDiv = document.createElement('div');
+    chartOuterDiv.className = 'mb-5 border border-gray-dark rounded-1 py-2';
+    var canvas = document.createElement('canvas');
+    canvas.id = 'lang_chart';
+    chartOuterDiv.appendChild(canvas);
+    divWrapper.appendChild(chartOuterDiv);
+
+    if (util.isAuthorPage()) {
+      var target = document.querySelector('.js-contribution-graph');
+      target.parentElement.insertBefore(divWrapper, target);
+    }
+
+    if (util.isOrganizationPage()) {
+
+      var targets = document.querySelectorAll('.text-normal');
+      var t = null;
+      targets.forEach(function(el) {
+        if (el.innerHTML === 'Top languages') {
+          t = el.parentElement;
+        }
+      });
+      t.parentNode.insertBefore(divWrapper, t);
+    }
+
+    return canvas;
+  }
+
+  return {
+    displayChart : function(repos) {
+      var model = new LanguageModel(repos);
+
+      var canvas = _createCanvasWithRelationalElements();
+      if (canvas) {
+        new Chart(canvas, {
+          type: 'doughnut',
+          data: {
+            labels: model.languages,
+            datasets: [
+              {
+                data: model.counts,
+                backgroundColor: model.colors,
+              }
+            ]
+          },
+          animation: {
+            animateScale: true
+          },
+          options: {
+            responsive: true,
+            onClick: function (event, legendItem) {
+              if (legendItem && legendItem.length > 0) {
+                var author = util.getTargetName();
+                var clickedLabel = legendItem[0]._model.label.toLowerCase();
+
+                var url = `https://github.com/${author}?utf8=%E2%9C%93&tab=repositories`;
+                if (clickedLabel !== 'others') {
+                  url += `&q=&type=&language=${clickedLabel}`;
+                }
+                window.location.href = url;
+              }
+            }
+          }
+        });
+      }
+    }
+  };
+}();
+
+module.exports = LanguageChart;
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -3,82 +3,7 @@
 var Storage = require('./Storage');
 var GitHubApi = require('./GitHubApi');
 var util = require('./util');
-var LanguageModel = require('./LanguageModel');
-
-function displayChart(repos) {
-  var model = new LanguageModel(repos);
-
-  var canvas = createCanvasWithRelationalElements();
-  if (canvas) {
-    new Chart(canvas ,{
-      type: 'doughnut',
-      data: {
-        labels: model.languages,
-        datasets: [
-          {
-            data: model.counts,
-            backgroundColor: model.colors,
-          }
-        ]
-      },
-      animation : {
-        animateScale : true
-      },
-      options : {
-        responsive : true,
-        onClick : function(event, legendItem) {
-          if (legendItem && legendItem.length > 0) {
-            var author = util.getTargetName();
-            var clickedLabel = legendItem[0]._model.label.toLowerCase();
-
-            var url = `https://github.com/${author}?utf8=%E2%9C%93&tab=repositories`;
-            if (clickedLabel !== 'others') {
-              url += `&q=&type=&language=${clickedLabel}`;
-            }
-            window.location.href = url;
-          }
-        }
-      }
-    });
-  }
-}
-
-function createCanvasWithRelationalElements() {
-  var divWrapper = document.createElement('div');
-
-  // show chartblock title
-  var chartBlockTitle = document.createElement('h2');
-  chartBlockTitle.className = 'f4 mb-2 text-normal';
-  chartBlockTitle.innerHTML = 'Language chart';
-  divWrapper.appendChild(chartBlockTitle);
-
-  // create chart canvas
-  var chartOuterDiv = document.createElement('div');
-  chartOuterDiv.className = 'mb-5 border border-gray-dark rounded-1 py-2';
-  var canvas = document.createElement('canvas');
-  canvas.id = 'lang_chart';
-  chartOuterDiv.appendChild(canvas);
-  divWrapper.appendChild(chartOuterDiv);
-
-  if (util.isAuthorPage()) {
-    var target = document.querySelector('.js-contribution-graph');
-    target.parentElement.insertBefore(divWrapper, target);
-  }
-
-  if (util.isOrganizationPage()) {
-
-    var targets = document.querySelectorAll('.text-normal');
-    var t = null;
-    targets.forEach(function(el) {
-      if (el.innerHTML === 'Top languages') {
-        t = el.parentElement;
-      }
-    });
-    t.parentNode.insertBefore(divWrapper, t);
-  }
-
-  return canvas;
-}
+var LanguageChart = require('./LanguageChart');
 
 function init () {
 
@@ -89,7 +14,7 @@ function init () {
     GitHubApi.getAuthorRepositories(token, util.getTargetName())
       .then(function (repos) {
         var author_repositories = Array.prototype.concat.apply([], repos);
-        displayChart(author_repositories);
+        LanguageChart.displayChart(author_repositories);
       })
       .catch(function (error) {
         console.error(error);
